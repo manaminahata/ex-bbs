83bd95d66137759de13f43600917cd0e05f75395
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-09 15:37:34 +0900

    リポジトリの作成

---
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,61 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Comment;
+
+@Repository
+public class CommentRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, i) -> {
+		Comment comment = new Comment();
+		comment.setId(rs.getInt("id"));
+		comment.setName(rs.getString("name"));
+		comment.setContent(rs.getString("content"));
+		comment.setArticleId(rs.getInt("article_id"));
+		return comment;
+	};
+	
+	/**
+	 * article_idを引数にとり、該当のデータを検索する
+	 * @param articleId
+	 * @return
+	 */
+	public List<Comment> findByArticleId(int articleId) {
+		String findByArticleIdSql = "SELECT * FROM articles WHERE article_id=:articleId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
+		List<Comment> commentList = template.query(findByArticleIdSql, param, COMMENT_ROW_MAPPER);
+		return commentList;
+	}
+	
+	/**
+	 * commentにレコードの追加
+	 * @param commnet
+	 */
+	public void insert(Comment commnet) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(commnet);
+		String insertSql = "INSERT INTO comments(name, comment, article_id) VALUES :name, :comment, :articleId";
+		template.update(insertSql, param);
+	}
+	
+	/**
+	 * レコードの削除
+	 * @param articleId
+	 */
+	public void deleteByArticleId(int articleId) {
+		String deleteByArticleIdSql = "DELETE FROM comments WHERE article_id=:articleId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("article_id", articleId);
+		template.update(deleteByArticleIdSql, param);
+	}
+	
+}
