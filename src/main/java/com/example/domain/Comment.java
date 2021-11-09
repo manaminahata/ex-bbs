87143ae39485d7ebb5adf00e009d4d03ce6b3004
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-09 14:30:23 +0900

    getter/setterの追加

--- src/main/java/com/example/domain/Comment.java
+++ src/main/java/com/example/domain/Comment.java
@@ -1,5 +1,46 @@
 package com.example.domain;
 
 public class Comment {
-
+	// id
+	private Integer id;
+	// コメント者名
+	private String name;
+	// コメント内容
+	private String content;
+	// 書き込みに対する番号
+	private Integer articleId;
+	
+	// getter/setterの定義
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Integer getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+	
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", name=" + name + ", content=" + content + ", articleId=" + articleId + "]";
+	}
+	
+	
+	
 }
